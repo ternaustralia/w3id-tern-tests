8508ecb75790bd157061950b5213da8c31a8e046
Edmond Chuc <edmond.chuc@outlook.com>
2020-07-16 15:05:42 +1000

    Added additional print statements and enabled checking of content-type header in response. Added new test rdf_turtle type. Added some new tests to test.yml

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,4 @@ RUN apt-get install apache2 -y
 
 RUN a2enmod rewrite
 
-RUN mkdir -p /var/www/html/tern/
-COPY .htaccess /var/www/html/tern/.htaccess
-COPY apache2.conf /etc/apache2/apache2.conf
-
-CMD service apache2 start && /bin/bash
+CMD service apache2 start && tail -f /dev/null
--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 # Author:   Edmond Chuc
-# Purpose:  Validate website and OWL ontology redirects. 
+# Purpose:  Validate persistent web and RDF redirects. 
 
 import requests
 import yaml
@@ -7,16 +7,14 @@ import yaml
 import time
 
 
-def test_website(url):
+def test_web(url):
     r = requests.get(url)
     assert r.status_code == 200 == requests.codes.ok, f'URL failed: {url}'
 
 
 def validate_response(r, url, media_type, request_type):
-    # It seems like the Content-Type response headers are still not being set correctly for some of the redirects.
-    # E.g. The query string for the org ontology with text/turtle works fine, but does not work with application/rdf+xml. Instead, it returns text/html.
-    # assert media_type in r.headers['Content-Type'], url + f" received Content-Type as {r.headers['Content-Type']} instead of {media_type}. Request type: {request_type}"
-    assert r.status_code == requests.codes.ok, url
+    assert media_type in r.headers['Content-Type'], url + f" received Content-Type as {r.headers['Content-Type']} instead of {media_type}. Request type: {request_type}"
+    assert r.status_code == requests.codes.ok, f'{url} with code {r.status_code} and message: {r.content.decode("utf-8")}'
 
 
 def validate_rdf(url, media_type, file_extension):
@@ -42,13 +40,29 @@ def validate_rdf(url, media_type, file_extension):
         validate_response(r, url, media_type, FILE_EXT)
 
 
-def test_ontology(url):
+def test_rdf(url):
+    print('testing for .ttl', flush=True)
     validate_rdf(url, 'text/turtle', '.ttl')
+    
+    print('testing for .rdf', flush=True)
     validate_rdf(url, 'application/rdf+xml', '.rdf')
+
+    print('testing for .nt', flush=True)
     validate_rdf(url, 'application/n-triples', '.nt')
+
+    print('testing for .jsonld', flush=True)
     validate_rdf(url, 'application/ld+json', '.jsonld')
+
+    print('testing for .n3', flush=True)
     validate_rdf(url, 'text/n3', '.n3')
+
+    print('testing for .owl', flush=True)
     validate_rdf(url, None, '.owl')
+
+
+def test_rdf_turtle(url):
+    print('testing for .ttl', flush=True)
+    validate_rdf(url, 'text/turtle', '.ttl')
 
 
 if __name__ == '__main__':
@@ -57,12 +71,18 @@ if __name__ == '__main__':
 
         start_time = time.time()
 
-        # Websites (HTML pages
-        for url in config['websites']:
-            test_website(f"{config['protocol']}://{config['docker_name']}/{url}")
+        # Web artifacts
+        for i, url in enumerate(config['web']):
+            print(f'testing web {i + 1} of {len(config["web"])} with {url}', flush=True)
+            test_web(f"{config['protocol']}://{config['docker_name']}/{url}")
 
-        # Ontologies
-        for url in config['ontologies']:
-            test_ontology(f"{config['protocol']}://{config['docker_name']}/{url}")
+        # RDF things
+        for i, url in enumerate(config['rdf']):
+            print(f'testing rdf {i + 1} of {len(config["rdf"])} with {url}', flush=True)
+            test_rdf(f"{config['protocol']}://{config['docker_name']}/{url}")
+
+        for i, url in enumerate(config['rdf_turtle']):
+            print(f'testing rdf_turtle {i + 1} of {len(config["rdf_turtle"])} with {url}', flush=True)
+            test_rdf_turtle(f"{config['protocol']}://{config['docker_name']}/{url}")
 
         print(f'All tests passed. Duration: {time.time() - start_time:.2f} seconds.')
